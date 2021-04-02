5f62525e3edf3790fd20a81ddfb860b53c105d92
jamieboy1337 <jamieboy1337@gmail.com>
2021-04-02 15:49:48 -0700

    account for asteroid rotation

--- cpp/src/AsteroidCollider.cpp
+++ cpp/src/AsteroidCollider.cpp
@@ -8,6 +8,12 @@ bool Collide(const Asteroid& asteroid, const Point2D& point) {
   float wind_distance = 0.0f;
   float theta_last, delta_theta;
   Point2D point_rel = (point - asteroid.position);
+  // transform point based on rotation
+  float rot_cos = cos(-asteroid.rotation);
+  float rot_sin = sin(-asteroid.rotation);
+  // point_rel is relative to asteroid center, but not rotated to account for asteroid's rotation.
+  point_rel = { point_rel.x * rot_cos + point_rel.y * rot_sin,
+                point_rel.x * -rot_sin + point_rel.y * rot_cos };
   Point2D delta = asteroid.geometry[asteroid.geometry.size() - 1] - point_rel;
   theta_last = atan2(delta.y, delta.x);
   for (int i = 0; i < asteroid.geometry.size(); i++) {
@@ -55,7 +61,7 @@ static Napi::Object Init(Napi::Env env, Napi::Object exports) {
   return exports;
 }
 
-NODE_API_MODULE(asteroidCollider, Init);
+NODE_API_MODULE(collidertest, Init);
 
 #endif
 
