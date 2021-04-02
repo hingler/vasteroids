83084daa2846b264bc080d7767f387af3a497d9e
jamieboy1337 <jamieboy1337@gmail.com>
2021-04-01 22:52:26 -0700

    update asteroidstest.ts

--- test/asteroidstest.ts
+++ test/asteroidstest.ts
@@ -1,9 +1,14 @@
 const AsteroidsTest = require("bindings")("asteroidstest");
-const expect = require("chai").expect;
+import { expect } from "chai";
+import { Asteroid } from "../addons-ts/Asteroid"
 
 describe("Asteroids", function() {
   it("Should return an asteroid on valid input", function() {
-    let ast = AsteroidsTest.generateAsteroid(1.5, 12);
+    let ast = AsteroidsTest.generateAsteroid(1.5, 12) as Asteroid;
     expect(ast.geometry.length).to.equal(12);
+    
+    for (let point of ast.geometry) {
+      expect(Math.sqrt(point.x * point.x + point.y * point.y)).to.be.lessThan(1.5);
+    }
   });
 });
