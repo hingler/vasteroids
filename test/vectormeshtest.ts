17bb58f42e8ef9f10c1812c10a642dda2b8b350d
jamieboy1337 <jamieboy1337@gmail.com>
2021-04-18 09:37:28 -0700

    add test for our vector mesh

---
+++ test/vectormeshtest.ts
@@ -0,0 +1,20 @@
+import { VectorMesh2D } from "../client/ts/gl/VectorMesh2D";
+import { expect } from "chai";
+
+describe("VectorMesh2D", function() {
+  it("Should properly maintain a list of vertices", function() {
+    let vec = new VectorMesh2D();
+    expect(vec.getIndexCount()).to.equal(0);
+    expect(vec.getVertexCount()).to.equal(0);
+    vec.addVertex([0, 0]);
+    expect(vec.getIndexCount()).to.equal(0);
+    expect(vec.getVertexCount()).to.equal(1);
+    vec.addVertex([2, 2]);
+    vec.addVertex([1, 1]);
+    expect(vec.getIndexCount()).to.equal(0);
+    expect(vec.getVertexCount()).to.equal(3);
+    vec.addTriangle([0, 1, 2]);
+    expect(vec.getIndexCount()).to.equal(3);
+    expect(vec.getVertexCount()).to.equal(3);
+  });
+});
